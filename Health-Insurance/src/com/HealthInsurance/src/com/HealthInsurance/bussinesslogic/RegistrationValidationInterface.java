e485cc3b2f38d1f07bed9e54814f436c846be1e0
GurramkondaYaswanth <50828371+GurramkondaYaswanth@users.noreply.github.com>
2021-04-18 01:29:06 +0530

    Delete Health-Insurance/src/com/HealthInsurance directory

--- Health-Insurance/src/com/HealthInsurance/src/com/HealthInsurance/bussinesslogic/RegistrationValidationInterface.java
+++
@@ -1,9 +0,0 @@
-package com.HealthInsurance.bussinesslogic;
-
-public interface RegistrationValidationInterface {
-	
-	public boolean checkUserDetails(String email, String password);
-	 boolean validPassword(String password, String confirmPassword);
-	  boolean validEmail(String email);
-
-}
